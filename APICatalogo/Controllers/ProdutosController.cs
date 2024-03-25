fc7d484ea2b951f949402d977af87d631e8f5eda
matthewsrsampaio <matthews_venom@hotmail.com>
2024-03-25 03:43:51 -0300

    Add Método Post e update no GET

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -21,8 +21,36 @@ public class ProdutosController : ControllerBase
         var produtos = _context.Produtos.ToList();
         if(produtos is null)
         {
-            return NotFound("Produtos não encontrados");
+            return NotFound("Produtos não encontrados!");
         }
         return produtos;
     }
+
+    [HttpGet("{id:int}", Name = "ObterProduto")] // "{id:int}") quer dizer que estou esperando um parâmetro do tipo inteiro chamado id
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        
+        if(produto is null)
+        {
+            return NotFound("Produto não encontrado!");
+        }
+        return produto;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        _context.Produtos.Add(produto); //Adicionando a instancia de produto
+        _context.SaveChanges(); //Persiste as informações
+
+
+        //O método CreatedAtRouteResult lança o código 201 e acessa pelo nome ObterProduto os dados do produto inserido
+        return new CreatedAtRouteResult("ObterProduto", //Criamos essa rota nomeavel para buscar o produto pelo ID.
+            new { id = produto.ProdutoId }, produto);
+    }
+
 }
